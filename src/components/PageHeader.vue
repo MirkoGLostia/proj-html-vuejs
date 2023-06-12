2772bb1686c50d16a6bb88c0d0464a35acddd760
MirkoGLostia <giggi.torre@mail.com>
2023-06-12 13:23:11 +0200

    prima impalcattura per header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -6,7 +6,44 @@ export default {
 
 <template>
     <header>
-        header part
+        <!-- componente di navigazione -->
+        <nav>
+            <div>
+                <!-- logo -->
+            </div>
+
+            <div>
+                <ul>
+                    <!-- link icone di navigazione, creare classi di selezione attiva (bonus: pensare ad un hover sulle icone) -->
+                </ul>
+            </div>
+        </nav>
+
+        <!-- componente jumbotron -->
+        <section>
+            <!-- immagini in background (da far ruotare con timer come bonus) -->
+            <div>
+                <!-- 3 titoli con stili diversi da rivedere alla fine pre bonus -->
+                <h3></h3>
+                <h1></h1>
+                <h2></h2>
+
+                <button>
+                    <!-- button da rivedere commissione per hover -->
+                </button>
+            </div>
+        </section>
+
+        <!-- sezione con messaggio di indirizzamento -->
+        <section>
+            <h3>
+                <!-- messaggio -->
+            </h3>
+
+            <button>
+                <!-- button da rivedere commissione per hover -->
+            </button>
+        </section>
     </header>
 </template>
 
