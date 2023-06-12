e2f2f4c51ce39189e4ee497cd2d1ebcb8dce1086
MirkoGLostia <giggi.torre@mail.com>
2023-06-12 13:36:53 +0200

    prima impalcatura footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -6,7 +6,101 @@ export default {
 
 <template>
     <footer>
-        footer part
+        <!-- componente mappa -->
+        <section>
+            <!-- mappa -->
+            <div>
+                <!-- serie di immagini prese da cartella img ricomposte a formare una mappa, sfondo con funzione di z-index -->
+            </div>
+            <div>
+                <!-- contenitore per div di interazione con la mappa, andrà in primo piano con z-index -->
+                <div>
+                    <!-- up/sx  -->
+                    <div>
+                        <!-- coordinate e link di sito della mappa -->
+                    </div>
+                    <div>
+                        <img src="" alt="">
+                    </div>
+
+                    <!-- dwn/sx -->
+                    <div>
+                        <!-- imagine mappa satellitare -->
+                        <img src="" alt="">
+                    </div>
+
+                    <!-- dwn/dx -->
+                    <div>
+                        <!-- controlli dello zoom -->
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- componente info -->
+        <section>
+            <!-- titolo -->
+            <div>
+                <h4></h4>
+                <span></span>
+                <img src="" alt="">
+            </div>
+
+            <!-- sub-component navigazione -->
+            <nav>
+                <!-- serie di link per ricavare informazioni -->
+                <div>
+                    <img src="" alt="">
+                    <h5></h5>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                <div>
+                    <img src="" alt="">
+                    <h5></h5>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                <div>
+                    <img src="" alt="">
+                    <h5></h5>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                <div>
+                    <img src="" alt="">
+                    <h5></h5>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </nav>
+
+            <!-- sub-component form messaggio diretto -->
+            <section>
+                <form>
+                    <div>
+                        <!-- sx info brevi -->
+                        <input type="text">
+                        <input type="text">
+                        <input type="text">
+                    </div>
+
+                    <div>
+                        <!-- dx messaggio diretto -->
+                        <input type="text">
+                    </div>
+                </form>
+
+                <button>submit</button>
+            </section>
+        </section>
     </footer>
 </template>
 
